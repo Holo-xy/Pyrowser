8d67d910c472b68095d6cb5d9ee1ba724a3b6e56
Mohammed Ashraf <mohammedashraf4599@gmail.com>
2024-05-05 21:53:14 +0300

    Fix: move headers to request

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ class URL:
             self.port = int(port)
 
     def request(self,):
-
         s = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
@@ -37,6 +36,8 @@ class URL:
         s.connect((self.host, self.port))
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
+        request += "Connection: close\r\n"
+        request += "User-Agent: " + f'Mozilla/5.0 ({os_name} {os_version}; {machine_type})\r\n'
         request += "\r\n"
         s.send(request.encode("utf8"))
         response = s.makefile("r", encoding="utf8", newline="\r\n")
@@ -50,8 +51,6 @@ class URL:
             response_headers[header.casefold()] = value.strip()
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
-        response_headers['Connection'] = 'close'
-        response_headers['User-Agent'] = f'Mozilla/5.0 ({os_name} {os_version}; {machine_type})'
         content = response.read()
         s.close()
         return content
